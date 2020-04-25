90a58fc071496c8f5a7a3aa0ed6c1b75753ee1d4
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-04-25 08:10:41 +0100

    Section for data extract from Excel spreadsheets
    Grouped existing functions to extract spreadsheet data.
    
    These functions are redundant with the new data.

--- src/practicalli/data_gov_uk.clj
+++ src/practicalli/data_gov_uk.clj
@@ -147,3 +147,77 @@
                      covid19-cases-uk-local-authority-district-date-specific))
 
 
+
+
+;; Extract data from Excel Spreadsheets
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(->> (spreadsheet/load-workbook-from-resource "data/historic-covid-19-dashboard-data.xlsx")
+     (spreadsheet/select-sheet "Countries")
+     (spreadsheet/select-cell "C9"))
+
+(def covid19-uk-coutry-data-cumulative-cases
+  (->> (spreadsheet/load-workbook-from-resource "data/historic-covid-19-dashboard-data.xlsx")
+       (spreadsheet/select-sheet "Countries")
+       spreadsheet/row-seq
+       (remove nil?)
+       (map spreadsheet/cell-seq)
+       (map #(map spreadsheet/read-cell %))))
+
+
+(defn add-dates
+  [data-set]
+  (let [dates (drop 3 (nth data-set 7))]
+    (mapv #(hash-map "day" %) dates)))
+
+(add-dates covid19-uk-coutry-data-cumulative-cases)
+
+
+(defn covid19-uk-countries-data-cumulative-cases-map
+  [data-set]
+  (let [country (clojure.string/trimr (second data-set))
+        values  (drop 3 data-set)]
+    (mapv #(hash-map "location" country  "cases" %) values) ))
+
+;; Using 3 to avoid nil values in Scotland, Wales and Northern Ireland countries.  4 is the first cell where all countries have a value.  Index of data starts at zero.
+
+;; Extract out specific rows of data
+;; Would have been easier if it were columns.
+
+(def covid19-uk-england-data-cumulative-cases
+  (map merge
+       (add-dates covid19-uk-coutry-data-cumulative-cases)
+       (covid19-uk-countries-data-cumulative-cases-map
+         (nth covid19-uk-coutry-data-cumulative-cases 8))))
+
+covid19-uk-england-data-cumulative-cases
+
+(def covid19-uk-scotland-data-cumulative-cases
+  (map merge
+       (add-dates covid19-uk-coutry-data-cumulative-cases)
+       (covid19-uk-countries-data-cumulative-cases-map
+         (nth covid19-uk-coutry-data-cumulative-cases 9))))
+
+(def covid19-uk-wales-data-cumulative-cases
+  (map merge
+       (add-dates covid19-uk-coutry-data-cumulative-cases)
+       (covid19-uk-countries-data-cumulative-cases-map
+         (nth covid19-uk-coutry-data-cumulative-cases 10))))
+
+(def covid19-uk-northern-ireland-data-cumulative-cases
+  (map merge
+       (add-dates covid19-uk-coutry-data-cumulative-cases)
+       (covid19-uk-countries-data-cumulative-cases-map
+         (nth covid19-uk-coutry-data-cumulative-cases 11))))
+
+(def covid19-uk-countries-all-cumulative-cases
+  (concat
+    covid19-uk-england-data-cumulative-cases
+    covid19-uk-scotland-data-cumulative-cases
+    covid19-uk-wales-data-cumulative-cases
+    covid19-uk-northern-ireland-data-cumulative-cases))
+
+;; TODO: refactor defs above to remove duplicate code,
+;; create a function to generate complete data.
+
+;; covid19-uk-countries-all-cumulative-cases
