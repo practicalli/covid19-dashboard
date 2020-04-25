c5689f3c1d170bd298f22b58556e9b4f7a91b07c
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-04-25 08:00:11 +0100

    Data transformation helpers
    Section for helpers that transform data once extracted into Clojure.
    
    - maximum-cases
    converts values into Integer types and finds the largest value, used for
    calculating the size of scale to use in the view

--- src/practicalli/data_gov_uk.clj
+++ src/practicalli/data_gov_uk.clj
@@ -14,6 +14,43 @@
             [dk.ative.docjure.spreadsheet :as spreadsheet]))
 
 
+;; Data Transformation helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn maximum-cases
+  "Calculates the maximum value of cases.
+  Used to calculate top end of scale in GeoJSON view"
+  [data-set]
+  (apply max
+         (map
+           #(Integer/parseInt
+              (get % "Cumulative lab-confirmed cases"))
+           data-set)))
+
+
+(defn combine-data-sets
+  [geo-json-data-set cases-data-set]
+
+  (update
+    geo-json-data-set
+    :features
+    (fn [features]  ;; as we are using update, features represents the whole geo-json data set
+      (mapv
+        (fn [feature]
+          (assoc
+            feature
+            :Cases
+            (get
+              (first
+                (filter
+                  #(some #{(:LAD13NM (:properties feature))} (vals %))
+                  cases-data-set))
+              "Cumulative lab-confirmed cases" -1)
+
+            :Location (:LAD13NM (:properties feature))))
+
+        features))))
+
 (defn covid19-uk-data
   [data-file]
   (let [data-extract
