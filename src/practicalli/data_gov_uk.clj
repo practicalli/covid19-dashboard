4aeff0cb0a83fd994fda8e82ba9cfbac2b88da24
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-04-25 07:57:05 +0100

    Require data format extraction libraries
    - semantic-csv to create clojure maps from data
    - jsonista to extract JSON into Clojure
    - spreadsheet to extract Excel workbooks into Clojure

--- src/practicalli/data_gov_uk.clj
+++ src/practicalli/data_gov_uk.clj
@@ -7,8 +7,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (ns practicalli.data-gov-uk
-  (:require [clojure.java.io :as io]
-            [clojure.data.csv :as csv]))
+  (:require [clojure.java.io   :as io]
+            [clojure.data.csv  :as csv]
+            [semantic-csv.core :as semantic-csv]
+            [jsonista.core     :as json]
+            [dk.ative.docjure.spreadsheet :as spreadsheet]))
 
 
 (defn covid19-uk-data
