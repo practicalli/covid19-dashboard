6ccf10e56bbdb204babf9ba049edc5b0dace3ccc
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-04-30 16:47:54 +0100

    Create general GeoJSON conversion function
    Replace def with a generic function that converts a given GeoJSON file to a
    Clojure data structure.
    
    Avoids duplication of code when several GeoJSON files are used (e.g. for
    Scotland, Wales, Northern Ireland)

--- src/practicalli/data_geo_json.clj
+++ src/practicalli/data_geo_json.clj
@@ -3,16 +3,31 @@
             [jsonista.core     :as json]))
 
 
-
-;; Extract data from GeoJSON files
+;; Convert GeoJSON files to Clojure for transformation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn geojson->clj
+  "Convert GeoJSON files to Clojure data structure
+
+  Arguments: GeoJSON file
+  Return: Clojure sequence"
+
+  [geo-json-resource]
+
+  (json/read-value
+    (io/resource geo-json-resource)
+    (json/object-mapper {:decode-key-fn true})))
+
+
 
 ;; GeoJSON of England Local Area Districts source:
 ;; https://raw.githubusercontent.com/martinjc/UK-GeoJSON/master/json/administrative/eng/lad.json
+;; Saved locally to resources/public/geo-data/uk-england-lad.geo.json
 
+
+;; Converted Data Sets
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def geojson-england-local-area-district
-  (json/read-value
-    (io/resource "public/geo-data/uk-england-lad.geo.json")
-    (json/object-mapper {:decode-key-fn true})))
-
+  (geojson->clj "public/geo-data/uk-england-lad.geo.json"))
