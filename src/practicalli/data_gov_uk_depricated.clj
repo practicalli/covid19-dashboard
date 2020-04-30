8e48f18e6a60173fe4d238e4f3e10e64528399d1
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-04-30 15:58:45 +0100

    New namespace - data-gov-uk-depreciated
    Create a new namespace for the code that is depreciated due to the change of
    data formats by Gov.uk.

---
+++ src/practicalli/data_gov_uk_depricated.clj
@@ -0,0 +1,6 @@
+(ns practicalli.data-gov-uk-depricated
+  (:require
+   [clojure.java.io   :as io]
+   [clojure.data.csv  :as csv]
+   [semantic-csv.core :as semantic-csv]
+   [dk.ative.docjure.spreadsheet :as spreadsheet]))
