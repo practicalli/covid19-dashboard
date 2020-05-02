061b30c328d0cfbcda8e796f7ede45893cf74a54
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-05-02 10:30:34 +0100

    Refactor dashboard view - separate sections into views
    Simply the main dashboard view by separating each section of the web page into
    its own view.
    
    Convert to a function and pass data for geographic view as an argument from the
    main function.
    
    Also makes the code reusable for a range of dashboards to be created.

--- src/practicalli/covid19_dashboard.clj
+++ src/practicalli/covid19_dashboard.clj
@@ -8,60 +8,84 @@
 
 (ns practicalli.covid19-dashboard
   (:gen-class)
-  (:require [oz.core :as oz]
-            [practicalli.vega-lite-components :as views]
-            [practicalli.view-helpers :as view-helpers]
-            [practicalli.data-gov-uk :as data-gov-uk]))
+  (:require
+   [oz.core                            :as oz]
+   [practicalli.vega-lite-views        :as views]
+   [practicalli.view-helpers           :as view-helpers]
+   [practicalli.data-gov-uk-depricated :as data-gov-uk-depricated]
+   [practicalli.data-gov-uk            :as data-gov-uk]
+   [practicalli.data-transform         :as data-transform]
+   [practicalli.data-geo-json          :as data-geo-json]))
 
 
-(def dashboard-corvid19-uk
+
+#_(defn dashboard-corvid19-uk
+    [geojson-view]
+    [:div
+     ;; Web page meta data
+     [:link {:rel  "stylesheet"
+             :href "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"}]
+
+     ;; Web page structure and content
+
+     ;; Heading
+     [:section {:class "hero is-dark is-bold"}
+      [:div {:class "hero-body"}
+       [:div {:class "container"}
+        [:h1 {:class "title is-family-primary"}
+         "COVID19 Tracker - Mock data"]
+        [:h2 {:class "subtitle"}
+         "Data will be extracted from "
+         [:a {:href "https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases"} "Gov.UK"]]]]]
+
+     ;; Daily Headline figures
+     [:section {:class "section has-text-centered"}
+      #_[:h1 (str "Headline figures for: " (get covid-uk-daily-indicators-map "DateVal"))]
+
+      ;; UK Totals
+      [:div {:class "columns"}
+       ;; UK cumulative totals
+       (view-helpers/headline-card-total
+         data-gov-uk-depricated/covid19-uk-data-latest-fixed
+         {:name "Cumulative UK Totals " :type "TotalUK"})
+
+       (view-helpers/headline-card-total
+         data-gov-uk-depricated/covid19-uk-data-latest-fixed
+         {:name "Daily Totals" :type "DailyUK"})]
+
+      ;; Country Cases
+      [:div {:class "columns"}
+       (map #(view-helpers/headline-card-country
+               data-gov-uk-depricated/covid19-uk-data-latest %)
+            [{:name "England" :alias "England"}
+             {:name "Scotland" :alias "Scotland"}
+             {:name "Wales" :alias "Wales"}
+             {:name "Northern Ireland" :alias "NI"}])]]
+
+     ;; Oz visualization
+     [:section {:class "section"}
+      [:vega-lite views/line-plot-uk-countries-cumulative-cases]
+      [:vega-lite views/stacked-bar-uk-countries-cumulative-cases]
+      [:vega-lite geojson-view]]])
+
+
+(defn dashboard-corvid19-uk
+  [geojson-view]
   [:div
-   ;; Web page meta data
-   [:link {:rel  "stylesheet"
-           :href "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"}]
-
    ;; Web page structure and content
+   view-helpers/include-bulma-css
 
    ;; Heading
-   [:section {:class "hero is-dark is-bold"}
-    [:div {:class "hero-body"}
-     [:div {:class "container"}
-      [:h1 {:class "title is-family-primary"}
-       "COVID19 Tracker - Mock data"]
-      [:h2 {:class "subtitle"}
-       "Data will be extracted from "
-       [:a {:href "https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases"} "Gov.UK"]]]]]
+   view-helpers/webpage-heading
 
    ;; Daily Headline figures
-   [:section {:class "section has-text-centered"}
-    #_[:h1 (str "Headline figures for: " (get covid-uk-daily-indicators-map "DateVal"))]
-
-    ;; UK Totals
-    [:div {:class "columns"}
-     ;; UK cumulative totals
-     (view-helpers/headline-card-total
-       data-gov-uk/covid19-uk-data-latest-fixed
-       {:name "Cumulative UK Totals " :type "TotalUK"})
-
-     (view-helpers/headline-card-total
-       data-gov-uk/covid19-uk-data-latest-fixed
-       {:name "Daily Totals" :type "DailyUK"})]
-
-    ;; Country Cases
-    [:div {:class "columns"}
-     (map #(view-helpers/headline-card-country
-             data-gov-uk/covid19-uk-data-latest %)
-          [{:name "England" :alias "England"}
-           {:name "Scotland" :alias "Scotland"}
-           {:name "Wales" :alias "Wales"}
-           {:name "Northern Ireland" :alias "NI"}])]]
+   (view-helpers/headline-figures)
 
    ;; Oz visualization
    [:section {:class "section"}
     [:vega-lite views/line-plot-uk-countries-cumulative-cases]
     [:vega-lite views/stacked-bar-uk-countries-cumulative-cases]
-    [:vega-lite views/geo-map-uk-england-local-area-districts-date-specific]
-    ]])
+    [:vega-lite geojson-view]]])
 
 
 (defn -main
