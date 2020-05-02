05f5f60ec7231904f085752f6f892c0ffc44236f
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-05-02 10:18:45 +0100

    Moved depreciated views to their own namespace
    Keeping the views for reference.

--- src/practicalli/vega_lite_components_depricated.clj
+++ src/practicalli/vega_lite_components_depricated.clj
@@ -1,5 +1,6 @@
-(ns practicalli.vega-lite-components-depricated)
-
+(ns practicalli.vega-lite-components-depricated
+  (:require
+   [practicalli.data-mock :as data-mock]))
 
 
 ;; Line plot
@@ -18,13 +19,13 @@
 ;; Stacked bar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-#_(def stacked-bar
-    {:mark     "bar"
-     :data     {:values (data-mock/covid19-data-set "England" "Scotland" "Wales" "Norther Ireland")}
-     :encoding {:x     {:field "day"
-                        :type  "ordinal"}
-                :y     {:aggregate "location"
-                        :field     "cases"
-                        :type      "quantitative"}
-                :color {:field "location"
-                        :type  "nominal"}}})
+(def stacked-bar
+  {:mark     "bar"
+   :data     {:values (data-mock/covid19-data-set "England" "Scotland" "Wales" "Norther Ireland")}
+   :encoding {:x     {:field "day"
+                      :type  "ordinal"}
+              :y     {:aggregate "location"
+                      :field     "cases"
+                      :type      "quantitative"}
+              :color {:field "location"
+                      :type  "nominal"}}})
