1449c07cb1ad844d0f85f760b2c91152652f84d3
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-05-02 10:39:19 +0100

    Delete previous dashboard view - clean main code
    Remove the dashboard def now the defn version works correctly.
    
    Change layout of -main to make more readable.

--- src/practicalli/covid19_dashboard.clj
+++ src/practicalli/covid19_dashboard.clj
@@ -18,57 +18,6 @@
    [practicalli.data-geo-json          :as data-geo-json]))
 
 
-
-#_(defn dashboard-corvid19-uk
-    [geojson-view]
-    [:div
-     ;; Web page meta data
-     [:link {:rel  "stylesheet"
-             :href "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"}]
-
-     ;; Web page structure and content
-
-     ;; Heading
-     [:section {:class "hero is-dark is-bold"}
-      [:div {:class "hero-body"}
-       [:div {:class "container"}
-        [:h1 {:class "title is-family-primary"}
-         "COVID19 Tracker - Mock data"]
-        [:h2 {:class "subtitle"}
-         "Data will be extracted from "
-         [:a {:href "https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases"} "Gov.UK"]]]]]
-
-     ;; Daily Headline figures
-     [:section {:class "section has-text-centered"}
-      #_[:h1 (str "Headline figures for: " (get covid-uk-daily-indicators-map "DateVal"))]
-
-      ;; UK Totals
-      [:div {:class "columns"}
-       ;; UK cumulative totals
-       (view-helpers/headline-card-total
-         data-gov-uk-depricated/covid19-uk-data-latest-fixed
-         {:name "Cumulative UK Totals " :type "TotalUK"})
-
-       (view-helpers/headline-card-total
-         data-gov-uk-depricated/covid19-uk-data-latest-fixed
-         {:name "Daily Totals" :type "DailyUK"})]
-
-      ;; Country Cases
-      [:div {:class "columns"}
-       (map #(view-helpers/headline-card-country
-               data-gov-uk-depricated/covid19-uk-data-latest %)
-            [{:name "England" :alias "England"}
-             {:name "Scotland" :alias "Scotland"}
-             {:name "Wales" :alias "Wales"}
-             {:name "Northern Ireland" :alias "NI"}])]]
-
-     ;; Oz visualization
-     [:section {:class "section"}
-      [:vega-lite views/line-plot-uk-countries-cumulative-cases]
-      [:vega-lite views/stacked-bar-uk-countries-cumulative-cases]
-      [:vega-lite geojson-view]]])
-
-
 (defn dashboard-corvid19-uk
   [geojson-view]
   [:div
@@ -94,16 +43,19 @@
 
   ;; Data Extraction and transformation pipleline
   (let [cases-data
-        (data-gov-uk/coronavirus-cases-data {:csv-file  "data-sets/uk-coronavirus-cases.csv"
-                                             :locations #{"Nation" "Country" "Region"}
-                                             :date      "2020-04-29"})
+        (data-gov-uk/coronavirus-cases-data
+          {:csv-file  "data-sets/uk-coronavirus-cases.csv"
+           :locations #{"Nation" "Country" "Region"}
+           :date      "2020-04-29"})
 
         geojson-cases-data
-        (data-geo-json/geojson-cases-data "public/geo-data/uk-local-area-districts-administrative-martinjc-lad.json"
-                                          cases-data)]
+        (data-geo-json/geojson-cases-data
+          "public/geo-data/uk-local-area-districts-administrative-martinjc-lad.json"
+          cases-data)]
 
     (oz/view!
-      (dashboard-corvid19-uk (views/geo-json-view geojson-cases-data 1000)))))
+      (dashboard-corvid19-uk
+        (views/geo-json-view geojson-cases-data 1000)))))
 
 
 ;; View Vega-lite components
